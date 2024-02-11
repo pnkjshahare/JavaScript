8da77ffa75efe32fc16f892095d6a891ddcc9f98
pnkjshahare <pankajsahare.04@gmail.com>
2024-02-11 11:28:30 +0530

    my commit

--- numberMath.js
+++ numberMath.js
@@ -1,8 +1,10 @@
 const balance=200;
-
+//convert no into string
 const newBalancce=balance.toString();
 console.log(newBalancce);
 console.log(typeof(newBalancce));
-
+//convert string into number
 const noInString=Number(newBalancce);
 console.log(typeof(noInString));
+//fix decimal point
+console.log(noInString.toFixed(3));
