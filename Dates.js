269b6755f49a828c91122a8685058b3008d3bafe
pnkjshahare <pankajsahare.04@gmail.com>
2024-02-11 12:29:19 +0530

    date

---
+++ Dates.js
@@ -0,0 +1,8 @@
+let myDate=new Date();
+console.log(myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+let timeStamp=Date.now();
+//given in miliseond
+console.log(timeStamp);
+console.log(myDate.getTime());
