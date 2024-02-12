80724c136725af037091e50e2331a0f064850558
pnkjshahare <pankajsahare.04@gmail.com>
2024-02-13 00:03:56 +0530

    obj

--- objectBasic.js
+++ objectBasic.js
@@ -1,12 +1,26 @@
 //singleton-->constrctor object
 
 //literal object
-
+const mysym=Symbol("key1");
 const JsUser={
     name :"pankaj",
     year: "TY",
     age :20,
-    add:"sahydri hostel"
+    add:"sahydri hostel",
+    [mysym]:"key2"
 }
 
 console.log(JsUser.name);
+console.log(JsUser["name"]);
+console.log(typeof(JsUser[mysym]));
+
+JsUser.name="pranay shahre"
+// Object.freeze(JsUser)
+JsUser.name="pankaj"
+console.log(JsUser);
+
+JsUser.greeting=function(){
+    console.log(`hello dear sir ${this.name}`);
+}
+
+JsUser.greeting();
