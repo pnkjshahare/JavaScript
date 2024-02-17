53ecf4f84843bf6532568b74f23518cf819aebbb
pnkjshahare <pankajsahare.04@gmail.com>
2024-02-17 20:26:28 +0530

    while loop

---
+++ LoopWhile.js
@@ -0,0 +1,16 @@
+// +++++++++++++ while Loops ++++++++++++
+console.log(`value of a`);
+let a=0;
+
+while(a<=10){
+    console.log(a);
+    a++;
+}
+
+// +++++++++++++++ Do while +++++++++
+console.log(`value of b`);
+let b=0
+do {
+    console.log(b);
+    b++
+} while (b<=10);
