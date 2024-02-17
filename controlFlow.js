f8008c23a8e64f468a8a8608d119907bf722e099
pnkjshahare <pankajsahare.04@gmail.com>
2024-02-17 19:06:22 +0530

    if-else

---
+++ controlFlow.js
@@ -0,0 +1,29 @@
+// let a="pankaj";
+// if(a=="pankaj"){
+//     console.log(`name=${a}`);
+// }
+
+
+//++++++++++++strick check++++++++++++++
+
+// if(2==="2"){
+//     console.log("equal");
+// }
+// else{
+//     console.log(" not equal");
+// }
+
+//+++++++++++++ nested ++++++++++
+let num=34;
+if(num>0&&num<=10){
+    console.log("range is 0 to 10");
+}
+else if(num>10&&num<=20){
+    console.log("range is 0 to 10");
+}
+if(num>20&&num<=30){
+    console.log("range is 20 to 30");
+}
+else if(num>30&&num<=40){
+    console.log("range is 30 to 40");
+}
